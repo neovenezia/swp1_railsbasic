4170e3d613fe4c7ea6036c9e144b241319f770c0
neovenezia <moumnetwork@gmail.com>
2015-10-01 07:05:33 +0000

    ruby

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+    def index
+    end
+    
+    def about
+    end
+
+    def practice
+    end
+    
+    def workspace
+    end
+    
+    def more
+    end
+end
